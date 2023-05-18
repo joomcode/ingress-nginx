21d9c04b6b888a6fa1f4e69a378bbd65caea8f18
Pavel Khotulev <p.kh@joom.com>
2023-05-18 19:31:54 +0300

    clean build script

--- joom-build.sh
+++ joom-build.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-export TAG="v1.3.1-batching-patch"
-export REGISTRY="jfrog.joom.it/docker-registry/joom-ingress-nginx"
-export ARCH="amd64"
+export REGISTRY="jfrog.joom.it/docker-registry/joom-ingress-nginx/controller"
 
-make build image
+export BASE_TAG
+BASE_TAG=$(cat TAG)
+export TAG="${BASE_TAG}-batching-patch"
 
-docker push $REGISTRY/controller:$TAG
+export BASE_IMAGE
+BASE_IMAGE=$(cat NGINX_BASE)
+
+export COMMIT
+COMMIT=$(git rev-parse --short HEAD)
+
+docker buildx build --no-cache --platform=linux/amd64 \
+--build-arg BASE_IMAGE="$BASE_IMAGE" \
+--build-arg VERSION="$TAG" \
+--build-arg COMMIT_SHA="$COMMIT" \
+--build-arg BUILD_ID=UNSET \
+ -t "${REGISTRY}:${TAG}" rootfs \
+ --load
+
+#docker push "${REGISTRY}:${TAG}"
